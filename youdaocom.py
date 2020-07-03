d8550b1fefdbd87ff82facd25746eb001f5b8561
qianqian <422374982qq@gmail.com>
2020-07-03 18:19:54 -0400

    翻译网站爬取

--- youdaocom.py
+++ youdaocom.py
@@ -8,7 +8,7 @@ while True:
     if content=='q!':
       break
 
-    url="http://fanyi.youdao.com/translate?smartresult=dict&smartresult=rule"
+    #url="http://fanyi.youdao.com/translate?smartresult=dict&smartresult=rule"
     #head={}
     #head["user-Agent"]="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.18362"
     data={}
